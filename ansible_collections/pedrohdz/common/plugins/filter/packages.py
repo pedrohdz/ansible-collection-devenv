e3b9ca882394dfa3e4fd1c9d903a1e418aa359aa
Pedro H <5179251+pedrohdz@users.noreply.github.com>
2021-05-10 13:36:30 +0200

    Added present_macport_variants

--- ansible_collections/pedrohdz/common/plugins/filter/packages.py
+++ ansible_collections/pedrohdz/common/plugins/filter/packages.py
@@ -3,8 +3,9 @@ from jinja2 import contextfilter
 
 
 def process_items(context, state_test, items):
-    filtered_items = filterfalse(ignore_test(context), items)
+    filtered_items = filterfalse(is_ignore(context), items)
     filtered_items = filter(state_test, filtered_items)
+    filtered_items = filterfalse(has_macport_variants(context), filtered_items)
     return sum(map(item_processor(context), filtered_items), start=[])
 
 
@@ -32,24 +33,35 @@ def process_item(context, item):
         return [item['name']]
 
 
+def process_macport_variants(context, items):
+    filtered_items = filterfalse(is_ignore(context), items)
+    filtered_items = filter(is_present, filtered_items)
+    filtered_items = filter(has_macport_variants(context), filtered_items)
+    return map(process_macport_variant_item, filtered_items)
+
+
+def process_macport_variant_item(item):
+    return (item['macports']['variant_name'], item['macports']['variant'])
+
+
 # -----------------------------------------------------------------------------
 # Filter tests
 # -----------------------------------------------------------------------------
-def absent_test(item):
+def is_absent(item):
     try:
         return item['state'] == 'absent'
     except (TypeError, KeyError):
         return False
 
 
-def present_test(item):
+def is_present(item):
     try:
         return item['state'] == 'present'
     except (TypeError, KeyError):
         return True
 
 
-def ignore_test(context):
+def is_ignore(context):
     def _ignore_filter(item):
         pkg_mgr = context['ansible_pkg_mgr']
         try:
@@ -59,22 +71,40 @@ def ignore_test(context):
     return _ignore_filter
 
 
+def has_macport_variants(context):
+    def _filter(item):
+        if context['ansible_pkg_mgr'] != 'macports':
+            return False
+        try:
+            return ('variant' in item['macports']
+                    or 'variant_name' in item['macports'])
+        except (KeyError, TypeError):
+            return False
+    return _filter
+
+
 # -----------------------------------------------------------------------------
 # Register filters
 # -----------------------------------------------------------------------------
 @contextfilter
 def absent_packages(context, items):
-    return process_items(context, absent_test, items)
+    return process_items(context, is_absent, items)
 
 
 @contextfilter
 def present_packages(context, items):
-    return process_items(context, present_test, items)
+    return process_items(context, is_present, items)
+
+
+@contextfilter
+def present_macport_variants(context, items):
+    return process_macport_variants(context, items)
 
 
 class FilterModule(object):
     def filters(self):
         return {
             'absent_packages': absent_packages,
+            'present_macport_variants': present_macport_variants,
             'present_packages': present_packages,
         }
